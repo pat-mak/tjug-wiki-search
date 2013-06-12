631e4d0c8594fc14d0e12845e55a858125961a79
Patryk Makuch <patryk.makuch@gmail.com>
2013-06-12 22:24:54 +0200

    search-title

--- src/pl/jug/trojmiasto/lucene/search/Searcher.java
+++ src/pl/jug/trojmiasto/lucene/search/Searcher.java
@@ -5,14 +5,16 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.analyzing.AnalyzingQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 
@@ -38,12 +40,14 @@ public class Searcher {
 		return searchResult;
 	}
 
-	private List<Article> extractArticlesFromTopDocs(TopDocs topDocs) throws IOException {
+	private List<Article> extractArticlesFromTopDocs(TopDocs topDocs)
+			throws IOException {
 		ScoreDoc[] scoreDocs = topDocs.scoreDocs;
 		List<Article> articles = new LinkedList<Article>();
 		for (ScoreDoc scoreDoc : scoreDocs) {
 			Document document = searcher.doc(scoreDoc.doc);
-			Article article = new Article(document.get(WikiIndexConfig.TITLE_FIELD_NAME),
+			Article article = new Article(
+					document.get(WikiIndexConfig.TITLE_FIELD_NAME),
 					document.get(WikiIndexConfig.CONTENT_FIELD_NAME),
 					document.get(WikiIndexConfig.CATEGORY_FIELD_NAME),
 					document.get(WikiIndexConfig.TIME_STRING_FIELD_NAME));
@@ -54,8 +58,19 @@ public class Searcher {
 
 	public SearchResult search(String query, int count) throws IOException {
 		SearchResult searchResult = new SearchResult();
-		TermQuery termQuery = new TermQuery(new Term(WikiIndexConfig.TITLE_FIELD_NAME, query));
-		TopDocs topDocs = searcher.search(termQuery, count);
+		AnalyzingQueryParser queryParser = new AnalyzingQueryParser(WikiIndexConfig.LUCENE_VERSION,
+				WikiIndexConfig.TITLE_FIELD_NAME, new StandardAnalyzer(
+						WikiIndexConfig.LUCENE_VERSION));
+		Query titleQuery = null;
+		try {
+			titleQuery = queryParser.parse(query);
+		} catch (ParseException e) {
+			searchResult.markFailed("Zapytanie było złe! i tory też...były złe, bo "+e.getMessage());
+			e.printStackTrace();
+			return searchResult;
+		}
+		
+		TopDocs topDocs = searcher.search(titleQuery, count);
 		searchResult.setArticles(extractArticlesFromTopDocs(topDocs));
 		return searchResult;
 	}
